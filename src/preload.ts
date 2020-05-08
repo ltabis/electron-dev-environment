7cc16b3b91a3f09ff5698128d7e99dbc0f51d78a
Lucas <lucas.tabis@epitech.eu>
2020-05-08 17:34:30 +0200

    :sparkles: Added a preload script to expose Node's ipc.

---
+++ src/preload.ts
@@ -0,0 +1,30 @@
+//* This file is used to expose node features  and create your personnal
+//* API to communicate with all processes.
+//* Customize it as you like.
+
+import { contextBridge, ipcRenderer } from "electron";
+
+//* Can be used to expose Node js features to the renderer process
+//* without exposing the entire library.
+contextBridge.exposeInMainWorld(
+    "process", {
+        send: (channel: string, ...args: any) => {
+
+            // whitelist channels
+            const validChannels = ["send-message"];
+
+            if (validChannels.includes(channel)) {
+                ipcRenderer.send(channel, ...args);
+            }
+        },
+        receive: (channel: string, func: any) => {
+
+            // whitelist channels
+            const validChannels = ["receiving-channel"];
+
+            if (validChannels.includes(channel)) {
+                ipcRenderer.on(channel, (event, ...args) => func(...args));
+            }
+        }
+    }
+);
---
+++ src/render.ts
@@ -0,0 +1,1 @@
+//* An example for a renderer process script.
